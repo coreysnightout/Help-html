6f7241921fe016bc7ab63161b7bdb7e01cfdfe11
Corey Redding and Leah Sherrell <leah@>
2017-04-24 16:30:33 -0700

    add ready function to js scriptand move css js and bootstrap to head in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-$("h1").click(function() {
-  alert("This is a header.");
-});
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
 
-$("p").click(function() {
-  alert("This is a paragraph.");
-});
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
 
-$("img").click(function() {
-  alert("This is an image.");
+  $("img").click(function() {
+    alert("This is an image.");
+  });
 });
